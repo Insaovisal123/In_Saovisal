6b51600894d36760a7865711b35e538d21d6445f
Saovisal In <5568296-INSAOVISAL@users.noreply.gitlab.com>
2020-07-22 18:28:48 +0700

    Final Commit

--- app/src/main/java/com/example/in_saovisal/MainActivity.java
+++ app/src/main/java/com/example/in_saovisal/MainActivity.java
@@ -6,6 +6,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +18,13 @@ import android.view.View;
 import com.example.in_saovisal.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    String s[];
+    int images[] = {R.drawable.myorder,
+            R.drawable.review,
+            R.drawable.setting,
+            R.drawable.changepassword,
+            R.drawable.logout};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
